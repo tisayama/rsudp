2212672e8b20237072a873cd4dad163eb5d0408a
Teppei ISAYAMA <teppei.isayama@gmail.com>
2025-05-10 23:07:20 +0900

    add pyqt5 to setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['obspy',
+                      'pyqt5>=5.0.0,<6',
                       'numpy>=2.0.0,<3',
                       'matplotlib>3.2,<4',
                       'pydub',
